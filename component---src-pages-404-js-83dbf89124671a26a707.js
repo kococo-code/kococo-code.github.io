ff23e59e6b91e08384e323fd71e553ed72224966
임진택 <kococo9504@gmail.com>
2020-09-20 14:36:57 +0900

    Updates

---
+++ component---src-pages-404-js-83dbf89124671a26a707.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{D604:function(n,t,e){},w2l6:function(n,t,e){"use strict";e.r(t);var o=e("q1tI"),a=e.n(o),u=(e("Bl7J"),e("vrFN"));e("D604");t.default=function(){return a.a.createElement(a.a.Fragment,null,a.a.createElement(u.a,{title:"404: Not found"}),a.a.createElement("section",{id:"NotFound"},"404: Not Found"))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-83dbf89124671a26a707.js.map
